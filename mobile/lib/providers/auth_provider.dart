f81ab39a34ec71f0cc34d0b7993ef8ce87367130
NassarAlshabi1 <adenmarina2@gmail.com>
2025-11-21 17:02:39 +0000

    Capy jam: fix provider imports
    Co-authored-by: Capy <capy@capy.ai>

--- mobile/lib/providers/auth_provider.dart
+++ mobile/lib/providers/auth_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../services/auth_local_store.dart';
+import '../services/auth_local_store.dart' show AuthLocalStore, AuthType;
 
 class AuthUser {
   final int id;
