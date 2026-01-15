13f508b899ec2b67ef35fccc2af680045f91c0e1
NassarAlshabi1 <adenmarina2@gmail.com>
2026-01-15 21:27:07 +0300

    Fix syntax error in google_drive_conflict_resolver.dart

--- mobile/lib/services/google_drive_conflict_resolver.dart
+++ mobile/lib/services/google_drive_conflict_resolver.dart
@@ -431,7 +431,7 @@ class GoogleDriveConflictResolver {
     }
     
     // 2. Perform Union: Add Exclusive Local Records (Non-Conflicting)
-    final tables = [ SyncConstants.allTablesInOrder;
+    final tables = [ SyncConstants.allTablesInOrder
     ];
 
     for (final tableName in tables) {
