78d44b4f4b592a591f8c8b6be313308a85f66e6f
NassarAlshabi1 <adenmarina2@gmail.com>
2026-02-11 01:42:53 +0000

    Revert "feat: تغيير الساعة الفاصلة من 14:00 إلى 6:00 صباحاً"
    This reverts commit 0d503e26617b0e2c45de1224b350f404c0b83633.

--- mobile/lib/services/booking_derived_fields_service.dart
+++ mobile/lib/services/booking_derived_fields_service.dart
@@ -303,7 +303,7 @@ class BookingDerivedFieldsService {
   List<_NightSegment> _buildNightSegments(
     DateTime checkin,
     DateTime checkout, {
-    int cutoffHour = 6,
+    int cutoffHour = 14,
   }) {
     final segments = <_NightSegment>[];
     var cursor = checkin;
--- mobile/lib/services/enhanced_booking_calculation_service.dart
+++ mobile/lib/services/enhanced_booking_calculation_service.dart
@@ -545,7 +545,7 @@ class EnhancedBookingCalculationService {
   List<_NightSegment> _buildNightSegments(
     DateTime checkin,
     DateTime checkout, {
-    int cutoffHour = 6,
+    int cutoffHour = 14,
   }) {
     final segments = <_NightSegment>[];
     var cursor = checkin;
