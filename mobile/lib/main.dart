97c21422db8c1625f68fcf4128fdac562cede43b
NassarAlshabi1 <adenmarina2@gmail.com>
2026-03-24 21:39:43 +0300

    Update main.dart

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -48,6 +48,7 @@ import 'services/google_drive_conflict_resolver.dart';
 import 'services/google_drive_unified_sync_coordinator.dart';
 import 'services/logging/log_models.dart';
 import 'services/diagnostics/diagnostics_logger.dart';
+import 'services/appwrite_delta_sycn.dart';
 import 'services/sync_queue_service.dart';
 import 'services/api_config_service.dart';
 import 'services/appwrite_config_manager.dart';
