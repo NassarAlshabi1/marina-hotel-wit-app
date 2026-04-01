9e4a1af79a34622b6675ac54c6864e3380d22759
Nassar <nassar@marina.com>
2026-04-01 15:39:54 +0000

    fix: hide GuestInfo from local_db import in settings_guests.dart
    Resolves build error: The getter 'bookings'/'name'/'phone'/'email'/'nationality'
    isn't defined for the type 'Object?' caused by ambiguous GuestInfo class
    from both local_db.dart and guest_info.dart

--- mobile/lib/screens/settings/settings_guests.dart
+++ mobile/lib/screens/settings/settings_guests.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../components/app_scaffold.dart';
 import '../../providers/repository_providers.dart';
-import '../../services/local_db.dart';
+import '../../services/local_db.dart' hide GuestInfo;
 import '../../services/sync_service.dart';
 import '../../utils/status_utils.dart';
 import 'guest_edit_screen.dart';
